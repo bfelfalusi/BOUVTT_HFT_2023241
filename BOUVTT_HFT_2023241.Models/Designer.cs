5e83996bd48c4f253d9d0baf9fd475dc1036b66b
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2023-11-06 10:36:47 +0100

    3 modell class done

---
+++ BOUVTT_HFT_2023241.Models/Designer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOUVTT_HFT_2023241.Models
+{
+    public class Designer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DesignerId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string DesignerName { get; set; }
+
+        public Designer()
+        {
+            
+        }
+
+        public Designer(string line)
+        {
+            string[] split = line.Split('#');
+            DesignerId = int.Parse(split[0]);
+            DesignerName = split[1];
+        }
+    }
+}
---
+++ BOUVTT_HFT_2023241.Models/Game.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOUVTT_HFT_2023241.Models
+{
+    public class Game
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GameId { get; set; }
+
+        [StringLength(50)]
+        public string GameName { get; set; }
+
+        [Range(1, 50000)]
+        public int PlayerCount { get; set; }
+
+        [Range(1, 5)]
+        public double Rating { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public int CompanyId { get; set; }
+
+        public virtual Company Company { get; set; }
+
+        public virtual ICollection<Designer> Designers { get; set; }
+
+
+        public Game()
+        {
+
+        }
+
+        public Game(string line)
+        {
+            string[] split = line.Split('#');
+            GameId = int.Parse(split[0]);
+            GameName = split[1];
+            PlayerCount = int.Parse(split[2]);
+            CompanyId = int.Parse(split[3]);
+            ReleaseDate = DateTime.Parse(split[4].Replace('*', '.'));
+            Rating = double.Parse(split[5]);
+        }
+    }
+}
