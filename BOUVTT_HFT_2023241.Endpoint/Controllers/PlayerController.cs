921ae153f3a2720ef25fb5588c75132759569b51
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2023-12-03 20:27:14 +0100

    Endpoint

---
+++ BOUVTT_HFT_2023241.Endpoint/Controllers/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using BOUVTT_HFT_2023241.Logic.Classes;
+using BOUVTT_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BOUVTT_HFT_2023241.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        PlayerLogic pl;
+
+        public PlayerController(PlayerLogic pl)
+        {
+            this.pl = pl;
+        }
+
+        
+        [HttpGet]
+        public IEnumerable<Player> ReadAll()
+        {
+            return pl.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Player Read(int id)
+        {
+            return pl.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Player value)
+        {
+            pl.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Player value)
+        {
+            pl.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            pl.Delete(id);
+        }
+
+    }
+}
--- BOUVTT_HFT_2023241.Endpoint/Startup.cs
+++ BOUVTT_HFT_2023241.Endpoint/Startup.cs
@@ -1,8 +1,17 @@
+using BOUVTT_HFT_2023241.Logic.Classes;
+using BOUVTT_HFT_2023241.Logic.Interfaces;
+using BOUVTT_HFT_2023241.Models;
+using BOUVTT_HFT_2023241.Repository;
+using BOUVTT_HFT_2023241.Repository.Interfaces;
+using BOUVTT_HFT_2023241.Repository.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +21,34 @@ namespace BOUVTT_HFT_2023241.Endpoint
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddTransient<PlayerDbContext>();
+
+            services.AddTransient<IRepository<Player>, PlayerRepository>();
+            services.AddTransient<IRepository<Team>, TeamRepository>();
+            services.AddTransient<IRepository<Coach>, CoachRepository>();
+            services.AddTransient<IRepository<Training>, TrainingRepository>();
+
+            services.AddTransient<IPlayerLogic, PlayerLogic>();
+            services.AddTransient<ITeamLogic, TeamLogic>();
+            services.AddTransient<ICoachLogic, CoachLogic>();
+            services.AddTransient<ITrainingLogic, TrainingLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BOUVTT_HFT_2023241.Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +57,17 @@ namespace BOUVTT_HFT_2023241.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BOUVTT_HFT_2023241.Endpoint v1"));
             }
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
