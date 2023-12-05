89dd62fc1241554455bda2cb8b5ce297ad2171fa
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2023-12-05 21:25:57 +0100

    finished

--- BOUVTT_HFT_2023241.Endpoint/Controllers/NonCrudController.cs
+++ BOUVTT_HFT_2023241.Endpoint/Controllers/NonCrudController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BOUVTT_HFT_2023241.Endpoint.Controllers
 {
