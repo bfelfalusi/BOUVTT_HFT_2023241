eb256d82f2577cc6c828c7fd95173168c5e6a250
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2023-12-05 16:01:26 +0100

    client restservice

--- BOUVTT_HFT_2023241.Client/Program.cs
+++ BOUVTT_HFT_2023241.Client/Program.cs
@@ -4,39 +4,175 @@ using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Channels;
 
 namespace BOUVTT_HFT_2023241.Client
 {
     internal class Program
     {
-
-        public static void Create(string entity) 
+        static RestService restservice;
+        static void Create(string entity)
         {
-            Console.WriteLine($"{entity} create");
+            if (entity == "Player")
+            {
+                Console.Write("Enter player's name: ");
+                string name = Console.ReadLine();
+                Console.Write("\nEnter player's height: ");
+                double height = double.Parse(Console.ReadLine());
+                Console.Write("\nEnter player's jerseynumber: ");
+                int jerseynum = int.Parse(Console.ReadLine());
+                restservice.Post(new Player()
+                {
+                    PlayerName = name,
+                    Height = height,
+                    JerseyNumber = jerseynum
+
+                },
+                    "player");
+            }
+            else if (entity == "Team")
+            {
+                Console.Write("Enter team's name: ");
+                string name = Console.ReadLine();
+                restservice.Post(new Team()
+                {
+                    TeamName = name
+                },
+                    "team");
+            }
+            else if (entity == "Coach")
+            {
+                Console.Write("Enter coach's position: ");
+                string pos = Console.ReadLine();
+                restservice.Post(new Coach()
+                {
+                    Position = pos
+                },
+                    "coach");
+            }
+            else if (entity == "Training")
+            {
+                Console.Write("Enter training's type: ");
+                string type = Console.ReadLine();
+                restservice.Post(new Training()
+                {
+                    TrainingType = type
+                },
+                    "training");
+            }
+        }
+        static void List(string entity)
+        {
+            if (entity == "Player")
+            {
+                List<Player> players = restservice.Get<Player>("player");
+                foreach (var item in players)
+                {
+                    Console.WriteLine(item.PlayerId + ": " + item.PlayerName);
+                }
+            }
+            else if (entity == "Team")
+            {
+                List<Team> teams = restservice.Get<Team>("team");
+                foreach (var item in teams)
+                {
+                    Console.WriteLine(item.TeamId + ": " + item.TeamName);
+                }
+            }
+            else if (entity == "Coach")
+            {
+                List<Coach> coaches = restservice.Get<Coach>("coach");
+                foreach (var item in coaches)
+                {
+                    Console.WriteLine(item.CoachId + ": " + item.Position);
+                }
+            }
+            else if (entity == "Training")
+            {
+                List<Training> trainings = restservice.Get<Training>("training");
+                foreach (var item in trainings)
+                {
+                    Console.WriteLine(item.TrainingId + ": " + item.TrainingType);
+                }
+            }
             Console.ReadLine();
         }
-
-        public static void Update(string entity)
+        static void Update(string entity)
         {
-            Console.WriteLine($"{entity} update");
-            Console.ReadLine();
+            if (entity == "Player")
+            {
+                Console.Write("Enter player's id to update: ");
+                int id = int.Parse(Console.ReadLine());
+                Player player = restservice.Get<Player>(id, "player");
+                Console.Write($"New name [old was: {player.PlayerName}]: ");
+                string name = Console.ReadLine();
+                player.PlayerName = name;
+                restservice.Put(player, "player");
+            }
+            else if (entity == "Team")
+            {
+                Console.Write("Enter team's id to update: ");
+                int id = int.Parse(Console.ReadLine());
+                Team team = restservice.Get<Team>(id, "team");
+                Console.Write($"New name [old was: {team.TeamName}]: ");
+                string name = Console.ReadLine();
+                team.TeamName = name;
+                restservice.Put(team, "team");
+            }
+            else if (entity == "Coach")
+            {
+                Console.Write("Enter coach's id to update: ");
+                int id = int.Parse(Console.ReadLine());
+                Coach coach = restservice.Get<Coach>(id, "coach");
+                Console.Write($"New position [old was: {coach.Position}]: ");
+                string pos = Console.ReadLine();
+                coach.Position = pos;
+                restservice.Put(coach, "coach");
+            }
+            else if (entity == "Training")
+            {
+                Console.Write("Enter training's id to update: ");
+                int id = int.Parse(Console.ReadLine());
+                Training training = restservice.Get<Training>(id, "training");
+                Console.Write($"New trainingtype [old was: {training.TrainingType}]: ");
+                string type = Console.ReadLine();
+                training.TrainingType = type;
+                restservice.Put(training, "training");
+            }
         }
-
-        public static void List(string entity)
+        static void Delete(string entity)
         {
-            Console.WriteLine($"{entity} list");
-            Console.ReadLine();
-        }
-
-        public static void Delete(string entity)
-        {
-            Console.WriteLine(entity + " delete");
-            Console.ReadLine();
+            if (entity == "Player")
+            {
+                Console.Write("Enter player's id to delete: ");
+                int id = int.Parse(Console.ReadLine());
+                restservice.Delete(id, "player");
+            }
+            else if (entity == "Team")
+            {
+                Console.Write("Enter team's id to delete: ");
+                int id = int.Parse(Console.ReadLine());
+                restservice.Delete(id, "team");
+            }
+            else if (entity == "Coach")
+            {
+                Console.Write("Enter coach's id to delete: ");
+                int id = int.Parse(Console.ReadLine());
+                restservice.Delete(id, "coach");
+            }
+            else if (entity == "Training")
+            {
+                Console.Write("Enter training's id to delete: ");
+                int id = int.Parse(Console.ReadLine());
+                restservice.Delete(id, "training");
+            }
         }
 
         static void Main(string[] args)
         {
+            restservice = new RestService("http://localhost:43388/", "player");
+
             var playerSubMenu = new ConsoleMenu(args, level: 1)
                 .Add("List", () => List("Player"))
                 .Add("Create", () => Create("Player"))
@@ -75,6 +211,6 @@ namespace BOUVTT_HFT_2023241.Client
             menu.Show();
         }
 
-        
+
     }
 }
